0b84e10b8ba624a1a6d34712a3f69d8b18236eb8
bertie <bertyvuster@gmail.com>
2019-03-13 02:22:31 +0300

    edit stage 4

--- src/main/java/games/Drunkard.java
+++ src/main/java/games/Drunkard.java
@@ -1,8 +1,44 @@
 package games;
 
 public class Drunkard {
+    private static final int PARS_TOTAL_COUNT = Par.values().length;
+
+    private static final int CARDS_TOTAL_COUNT = PARS_TOTAL_COUNT * Suit.values().length;
 
     public static void main(String... __) {
         System.out.println("Hello world!");
     }
+
+    enum Suit {
+        SPADES, // пики
+        HEARTS, // червы
+        CLUBS, // трефы
+        DIAMONDS // бубны
+    }
+
+    enum Par {
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK, // Валет
+        QUEEN, // Дама
+        KING, // Король
+        ACE // Туз
+    }
+
+    private static Suit getSuit(int cardNumber) {
+        return Suit.values()[cardNumber / PARS_TOTAL_COUNT];
+    }
+
+    private static Par getPar(int cardNumber) {
+        return Par.values()[cardNumber % PARS_TOTAL_COUNT];
+    }
+
+    private static String toString(int cardNumber) {
+        return getPar(cardNumber) + " " + getSuit(cardNumber);
+    }
+
+
 }
